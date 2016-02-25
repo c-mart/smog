4e1d87703eff25af80813e124c78bef7c77012ac
chris <chris@c-mart.in>
2016-02-25 11:45:25 -0700

    Refactored test suite with helper method for creating a post. Created test case to ensure permalinks are sanitized.

--- smog_test.py
+++ smog_test.py
@@ -20,12 +20,23 @@ class smogTestCase(unittest.TestCase):
         smog.db.session.remove()
         smog.db.drop_all()
 
-    def login(self, email, password):
+    # Helper Methods
+    def login(self, email=test_user_email, password=test_user_password):
         return self.app.post('/login', data=dict(email=email, password=password), follow_redirects=True)
 
     def logout(self):
         return self.app.get('/logout', follow_redirects=True)
 
+    def create_post(self,
+                    title='Test post',
+                    body='The quick brown fox jumps over the lazy dog',
+                    description='Test description',
+                    permalink='test-permalink',
+                    published=True,
+                    comments_allowed=True):
+        return self.app.post('/create', data=locals(), follow_redirects=True)
+
+    # Test Cases
     def test_no_posts(self):
         r = self.app.get('/')
         assert 'No posts yet.' in r.data, "We should see no posts"
@@ -33,7 +44,7 @@ class smogTestCase(unittest.TestCase):
     def test_login_logout(self):
         r = self.login('invalid', 'credentials')
         assert 'Invalid email or password, try again.' in r.data, "We should receive an invalid credentials message"
-        r = self.login(self.test_user_email, self.test_user_password)
+        r = self.login()
         assert 'You are logged in.' in r.data, "We should receive a logged in notice"
         r = self.logout()
         assert 'You are logged out.' in r.data, "We should receive a logged out notice"
@@ -44,12 +55,8 @@ class smogTestCase(unittest.TestCase):
 
     def test_rate_limit_login(self):
         smog.limiter.enabled = True
-        self.login('invalid', 'credentials')
-        self.login('invalid', 'credentials')
-        self.login('invalid', 'credentials')
-        self.login('invalid', 'credentials')
-        self.login('invalid', 'credentials')
-        self.login('invalid', 'credentials')
+        for x in range(6):
+            self.login('invalid', 'credentials')
         r = self.login('invalid', 'credentials')
         assert 'You have tried doing that too often' in r.data, "We should see a rate limit warning."
         r = self.login(self.test_user_email, self.test_user_password)
@@ -57,61 +64,33 @@ class smogTestCase(unittest.TestCase):
         assert 'You have tried doing that too often' in r.data, "We should see a rate limit warning."
 
     def test_compose_post(self):
-        self.login(self.test_user_email, self.test_user_password)
+        self.login()
         r = self.app.get('/create')
-        assert '<h1>Create Post</h1>' in r.data
+        assert '<h2>Create Post</h2>' in r.data
 
     def test_create_post(self):
-        self.login(self.test_user_email, self.test_user_password)
-        r = self.app.post('/create', data=dict(
-            title='Test post',
-            body='The quick brown fox jumps over the lazy dog',
-            description='Test description',
-            permalink='test-permalink',
-            published=True,
-            comments_allowed=True
-        ), follow_redirects=True)
+        self.login()
+        r = self.create_post()
         assert 'The quick brown fox jumps over the lazy dog' in r.data, "We should see post body"
         assert 'Posted by Test User' in r.data, "We should see name of user posting"
         assert 'on ' + datetime.utcnow().strftime('%Y-%m-%d') in r.data, "We should see creation date"
 
     def test_auto_permalink(self):
-        self.login(self.test_user_email, self.test_user_password)
-        r = self.app.post('/create', data=dict(
-            title='Test post',
-            body='The quick brown fox jumps over the lazy dog',
-            description='',
-            permalink='',
-            published=True,
-            comments_allowed=True
-        ), follow_redirects=True)
+        self.login()
+        r = self.create_post(permalink='')
         assert '<a href="/posts/test-post">' in r.data, "We should see automatically generated permalink"
 
     def test_follow_permalink(self):
-        self.login(self.test_user_email, self.test_user_password)
-        r = self.app.post('/create', data=dict(
-            title='Test post',
-            body='The quick brown fox jumps over the lazy dog',
-            description='Test description',
-            permalink='test-permalink',
-            published=True,
-            comments_allowed=True
-        ), follow_redirects=True)
+        self.login()
+        r = self.create_post()
         assert '<a href="/posts/test-permalink">' in r.data, "We should see link for post permalink"
         # Following permalink
         r = self.app.get('/posts/test-permalink')
         assert 'The quick brown fox jumps over the lazy dog' in r.data, "We should see post body"
 
     def test_cannot_access_permalink_unpublished(self):
-        self.login(self.test_user_email, self.test_user_password)
-        self.app.post('/create', data=dict(
-            title='Test post',
-            body='The quick brown fox jumps over the lazy dog',
-            description='Test description',
-            permalink='test-permalink',
-            published=False,
-            comments_allowed=True
-        ), follow_redirects=True)
+        self.login()
+        self.create_post(published=False)
         # Following permalink
         r = self.app.get('/posts/test-permalink')
         assert 'The quick brown fox jumps over the lazy dog' in r.data,\
@@ -120,19 +99,18 @@ class smogTestCase(unittest.TestCase):
         r = self.app.get('/posts/test-permalink')
         assert r.status_code == 404, "Trying to access unpublished post while not logged in should get us a 404"
 
+    def test_clean_up_permalink(self):
+        self.login()
+        self.create_post(permalink="This: is An ugly/dirty permalink --")
+        r = self.app.get('/posts/this-is-an-ugly-dirty-permalink')
+        assert 'The quick brown fox jumps over the lazy dog' in r.data, "Permalink was not sanitized"
+
     def test_edit_post(self):
-        self.login(self.test_user_email, self.test_user_password)
-        r = self.app.post('/create', data=dict(
-            title='Test post',
-            body='The quick brown fox jumps over the lazy dog',
-            description='Test description',
-            permalink='test-permalink',
-            published=True,
-            comments_allowed=True
-        ), follow_redirects=True)
+        self.login()
+        r = self.create_post()
         post_id = re.search("/create\?id=([0-9]+)", r.data).group(1)
         r = self.app.get('/create?id=' + str(post_id))
-        assert '<h1>Edit Post</h1>' in r.data, "We should see edit page"
+        assert '<h2>Edit Post</h2>' in r.data, "We should see edit page"
         r = self.app.post('/create', data=dict(
             update_id=post_id,
             title='Test post',
@@ -147,28 +125,15 @@ class smogTestCase(unittest.TestCase):
         assert 'The quick brown fox jumps over the lazy dog' not in r.data, "We should see edited post body"
 
     def test_create_unpublished_post_then_publish(self):
-        self.login(self.test_user_email, self.test_user_password)
-        r = self.app.post('/create', data=dict(
-            title='Test post',
-            body='The quick brown fox jumps over the lazy dog',
-            description='Test description',
-            permalink='test-permalink',
-            comments_allowed=True
-        ), follow_redirects=True)
+        self.login()
+        r = self.create_post(published=False)
         assert 'No posts yet.' in r.data, "We should see no published posts"
         r = self.app.get('/unpublished')
         assert 'The quick brown fox jumps over the lazy dog' in r.data, "We should see post in /unpublished"
 
     def test_delete_post(self):
-        self.login(self.test_user_email, self.test_user_password)
-        r = self.app.post('/create', data=dict(
-            title='Test post',
-            body='The quick brown fox jumps over the lazy dog',
-            description='Test description',
-            permalink='test-permalink',
-            published=True,
-            comments_allowed=True
-        ), follow_redirects=True)
+        self.login()
+        r = self.create_post()
         post_id = re.search("/create\?id=([0-9]+)", r.data).group(1)
         r = self.app.get('/create?id=' + str(post_id))
         assert re.search("<a href=\"/delete/[0-9]+\">Delete post</a>", r.data) is not None, "Delete link is missing"
@@ -178,42 +143,20 @@ class smogTestCase(unittest.TestCase):
 
     def test_cannot_create_duplicate_post(self):
         # Try to create two identical posts, we should not be able to
-        self.login(self.test_user_email, self.test_user_password)
-        self.app.post('/create', data=dict(
-            title='Test post',
-            body='The quick brown fox jumps over the lazy dog',
-            description='Test description',
-            permalink='test-permalink',
-            published=True,
-            comments_allowed=True
-        ), follow_redirects=True)
-        r = self.app.post('/create', data=dict(
-            title='Test post',
-            body='The quick brown fox jumps over the lazy dog',
-            description='Test description',
-            permalink='test-permalink',
-            published=True,
-            comments_allowed=True
-        ), follow_redirects=True)
+        self.login()
+        self.create_post()
+        r = self.create_post()
         assert 'There was a problem creating your post.' in r.data, "User should receive error message"
         r = self.app.get('/')
         assert r.data.count("<h2 class=\"post-title\"><a href=\"/posts/test-permalink\">Test post</a></h2>") == 1,\
             "Duplicate posts exist where they should not"
 
     def test_markdown_parsing(self):
-        self.login(self.test_user_email, self.test_user_password)
-        r = self.app.post('/create', data=dict(
-            title='Test post',
-            # Test Markdown input
-            body='### Heading\n'
-                 '- Bullet\n\n'
-                 'Paragraph\n\n'
-                 '    some_code()',
-            description='Test description',
-            permalink='test-permalink',
-            published=True,
-            comments_allowed=True
-        ), follow_redirects=True)
+        self.login()
+        r = self.create_post(body='### Heading\n'
+                             '- Bullet\n\n'
+                             'Paragraph\n\n'
+                             '    some_code()',)
         assert all(x in r.data for x in
                    ['<h3>Heading</h3>', '<li>Bullet</li>', '<p>Paragraph</p>', '<code>some_code()\n</code>']
                    ), "Markdown not parsed correctly"
